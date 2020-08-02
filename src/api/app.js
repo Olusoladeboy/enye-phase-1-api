e35dac2765e6d7a231cb3ac9f86831527a911dde
olusoladeboy <goldblumie1@gmail.com>
2020-08-02 16:06:44 +0100

    Configured Server Files

---
+++ src/api/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+
+const app = express()
+
+const hostname = "localhost"; // "127.0.0.1";
+const port = process.env.PORT || 5000;
+
+app.get('', (req, res) => {
+    res.json({
+        status: "Working"
+    })
+})
+
+// listen for requests
+const server = app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+setInterval(() => server.getConnections((err, connections) => console.log(`${connections} connections currently open`)), 10000);
+
+let connections = [];
+
+server.on("connection", (connection) => {
+    connections.push(connection);
+    connection.on("close", () => connections = connections.filter(curr => curr !== connection));
+});
+
+export default app;
